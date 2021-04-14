fa15c2488392792ebfafd134de1f43b4af6a9b91
sgash708 <sugash7081023@gmail.com>
2021-04-14 09:55:33 +0900

    firstcommit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	// h is host?
+	go h.run()
+
+	router := gin.New()
+	// read html
+	router.LoadHTMLFiles("index.html")
+
+	router.GET("/room/:roomId", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	router.GET("/ws/:roomId", func(c *gin.Context) {
+		roomId := c.Param("roomId")
+		serverWs(c.Writer, c.Request, roomId)
+	})
+
+	router.Run("0.0.0.0:9000")
+}
